bb05cc794b137016656d8d6bea07d37937d13112
weevood <5528482+weevood@users.noreply.github.com>
2026-01-12 17:33:48 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+"""
+Global configuration for ART scripts.
+All tunable constants should be defined here.
+"""
+
+# Script names
+UPLOAD_SCRIPT = "_art_upload.py"
+SET_SCRIPT = "_art_set.py"
+
+# Execution interval (in seconds)
+INTERVAL = 60
